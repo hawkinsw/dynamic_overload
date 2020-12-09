4b23ec2bcdb5066f132e125a6e1ff5b7a98d7740
Will Hawkins <hawkinsw@obs.cr>
2020-12-08 23:57:08 -0500

    Variable number of parameters

--- dynamic.cpp
+++ dynamic.cpp
@@ -11,47 +11,65 @@ class Derived1 : public Base {};
 
 class Derived2 : public Base {};
 
-template <typename return_type>
+template <class ...Args>
+auto variable_tuple_of_type_indexes() {
+	return std::make_tuple(std::type_index(typeid(Args))...);
+}
+
+template <typename return_type, class ...Args>
 class Override {
 	public:
-	template <class PT1, class PT2>
-	void setOverride(return_type (*override)(PT1 *, PT2 *)) {
-		auto pair = std::make_pair(std::type_index(typeid(PT1)), std::type_index(typeid(PT2)));
-		m_funcs[pair] = (return_type (*)(void*, void*))override;
+	template <class ...FunctionArgs>
+	void setOverride(return_type (*override)(FunctionArgs...)) {
+		auto tuple = std::make_tuple(std::type_index(typeid(FunctionArgs))...);
+		m_funcs[tuple] = (return_type (*)(...))override;
 	}
 
-	template <class PT1, class PT2>
-  return_type operator()(PT1 *p1, PT2 *p2) {
-		auto pair = std::make_pair(std::type_index(typeid(PT1)), std::type_index(typeid(PT2)));
-    auto func = m_funcs[pair];
-		return func((void*)p1, (void*)p2);
+	template <class ...FunctionArgs>
+  return_type operator()(FunctionArgs... args) {
+		auto tuple = std::make_tuple(std::type_index(typeid(FunctionArgs))...);
+    auto func = m_funcs[tuple];
+		return func(args...);
   }
 
 	private:
-  std::map<std::pair<std::type_index, std::type_index>,
-           return_type (*)(void*, void*)>
+  std::map<decltype(variable_tuple_of_type_indexes<Args...>()),
+           return_type (*)(...)>
       m_funcs;
 };
 
-float testing_base(Base *x, Base *y) {
+float testing_base(Base *x, Base *y, Base *z) {
 	std::cout << "testing base?\n";
 	return 1.0f;
 }
 
-float testing_derived(Base *x, Derived1 *y) {
-	std::cout << "testing derived?\n";
+float testing_derived1(Derived1 *x, Derived1 *y, Derived1 *z) {
+	std::cout << "testing derived1?\n";
+	return 1.0f;
+}
+
+float testing_derived2(Derived2  *x, Derived2 *y, Derived2 *z) {
+	std::cout << "testing derived2?\n";
 	return 1.0f;
 }
 
 int main() {
   std::cout << "Hello, world.\n";
-	Base one, two;
-	Derived1 three;
-	Override<float> over;
-	over.setOverride(testing_base);
-	over.setOverride(testing_derived);
+	Base base;
+	Derived1 derived1;
+	Derived2 derived2;
 
-	over(&one, &two);
-	over(&one, &three);
+	Override<float, Base*, Base*, Base *> over;
+	over.setOverride(testing_base);
+	over.setOverride(testing_derived1);
+	over.setOverride(testing_derived2);
+
+	over(&base, &base, &base);
+	over(&derived1, &derived1, &derived1);
+	over(&derived2, &derived2, &derived2);
+	over(&derived2, &derived2, &derived2);
+	over(&derived1, &derived1, &derived1);
+	over(&base, &base, &base);
+
   return 1;
 }
